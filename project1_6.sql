eb86eabf2d49303011d80dfcd8b3a9f8239fd371
mariiagiu <maria13799@gmail.com>
2024-06-12 19:19:38 +0400

    Update project1_6.sql

--- project1_6.sql
+++ project1_6.sql
@@ -1,9 +1,8 @@
 SELECT 
 	quantity,
 	product_category,
-    SUM (total_amount) AS sum_total_amount,
-	AVG (price_per_unit) AS avg_price_per_unit
+	gender,
+	COUNT(customer_id)
 FROM project_1
-	WHERE product_category = 'Electronics'
-GROUP BY quantity, product_category
-ORDER BY quantity
+GROUP BY quantity, product_category,gender
+ORDER BY quantity DESC
