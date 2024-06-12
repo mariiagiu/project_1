5ce15adb6dbc8b48487028038a0ec087520180d8
mariiagiu <maria13799@gmail.com>
2024-06-12 19:20:12 +0400

    Update project1_7.sql

--- project1_7.sql
+++ project1_7.sql
@@ -1,15 +1,8 @@
-SELECT
-   product_category,
-   price_per_unit,
-   quantity,
-   total_amount
+SELECT 
+    quantity,
+    product_category,
+    SUM(price_per_unit) AS sum_price_per_unit,
+    AVG(price_per_unit) AS avg_price_per_unit
 FROM project_1
-WHERE quantity = 1
-GROUP BY product_category,price_per_unit
-ORDER BY product_category,price_per_unit DESC
-
-
-
-
-
-
+GROUP BY quantity, product_category
+ORDER BY quantity DESC
